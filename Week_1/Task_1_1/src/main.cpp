40ec914c351d87a3e42c52f5f63556df7e374459
Tati Markova <tatiana.e.markova@gmail.com>
2020-02-20 17:02:11 +0100

    Task_1_1

---
+++ Week_1/Task_1_1/src/main.cpp
@@ -0,0 +1,31 @@
+/****************************
+ * Coursera                 *
+ * White belt               *
+ * @autor: Tatiana Markova  *
+ * task: 1_1                *
+ ****************************/
+
+#include <iostream>
+#include <cstdlib>
+
+/************
+ * Input:   *
+ *   2   3  *
+ *   -4  -9 *
+ *   -1  2  *
+ ************/
+
+int main(int argv, char* argc[]) {
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << a + b << '\n';
+
+    return EXIT_SUCCESS;
+}
+
+/************
+ * Output:  *
+ *   5      *
+ *   -13    *
+ *   1      *
+ ************/
