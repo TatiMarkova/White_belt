c1461bf241b9dd229bd48f9c6fd84abfd4462349
Tati Markova <tatiana.e.markova@gmail.com>
2020-02-20 19:24:43 +0100

    Task_1_5

---
+++ Week_1/Task_1_5/src/main.cpp
@@ -0,0 +1,49 @@
+/****************************
+ * Coursera                 *
+ * White belt               *
+ * @autor: Tatiana Markova  *
+ * task: 1_5                *
+ ****************************/
+
+#include <iostream>
+#include <cstdlib>
+
+/*****************************
+ * Input:                    *
+ *   100  110  120  5  10    *
+ *   115  110  120  5  10    *
+ *   150  110  120  5  12.5  *
+ *    N    A    B   X   Y    *
+ *****************************/
+
+int main(int argv, char* argc[]) {
+    double N, A, B, X, Y;
+    std::cin >> N >> A >> B >> X >> Y;
+    
+    //version_1
+    /*if (N > B)  
+        std::cout << N - (N / 100 * Y) << '\n';
+    else if (N > A) 
+        std::cout << N - (N / 100 * X) << '\n';
+    else 
+        std::cout << N << '\n';
+    */
+
+    //version_2
+    if (N > B)  
+        N -= N / 100 * Y;
+    else if (N > A) 
+        N -= N / 100 * X;
+   
+    std::cout << N << '\n';
+
+
+    return EXIT_SUCCESS;
+}
+
+/**************
+ * Output:    *
+ *   100      *
+ *   109.25   *
+ *   131.25   *
+ **************/
