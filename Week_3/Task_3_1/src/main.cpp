49b4a098a6e5724c761fe759cf031f737cbd9bb4
Tati Markova <tatiana.e.markova@gmail.com>
2020-02-28 15:16:36 +0100

    Task_3_1

---
+++ Week_3/Task_3_1/src/main.cpp
@@ -0,0 +1,41 @@
+/****************************
+ * Coursera                 *
+ * White belt               *
+ * @autor: Tatiana Markova  *
+ * task: 3_1                *
+ ****************************/
+
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+/****************************
+ * Input:                   *
+ *  2 -4 3                  *
+ *  3 1 -3 2                *
+ ****************************/
+
+int main(int argv, char* argc[]) {
+    int n;
+    std::cin >> n;
+    std::vector<int> nums(n);
+    for (int& i : nums) {
+        std::cin >> i;
+    }
+    sort(begin(nums), end(nums), [](const auto& l, const auto& r) {
+        return std::abs(l) < std::abs(r);
+    });
+    for (int& i : nums) {
+        std::cout << i << ' ';
+    }
+    std::cout << '\n';
+    return EXIT_SUCCESS;
+}
+
+/*************
+ * Output:   *
+ *   3 -4    *
+ *   1 2 -3  *
+ *************/
