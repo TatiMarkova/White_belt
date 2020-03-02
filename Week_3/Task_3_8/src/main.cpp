a9e588e04f09e2200672904f6025b03bb6df6c21
Tati Markova <tatiana.e.markova@gmail.com>
2020-03-02 18:03:27 +0100

    Task_3_8

---
+++ Week_3/Task_3_8/src/main.cpp
@@ -0,0 +1,22 @@
+/****************************
+ * Coursera                 *
+ * White belt               *
+ * @autor: Tatiana Markova  *
+ * task: 3_8                *
+ ****************************/
+#include <cstdlib>
+
+class Incognizable {
+public:
+    int x = 0;
+    int y = 0;
+};
+
+int main(int argv, char* argc[]) {
+    Incognizable a;
+    Incognizable b = {};
+    Incognizable c = {0};
+    Incognizable d = {0, 1};
+        
+    return EXIT_SUCCESS;
+}
